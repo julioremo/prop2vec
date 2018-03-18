7f31e5b4548c3b0bbb196ba8576e0bda0e20ab62
julioremo <et.julio@gmail.com>
2018-03-18 18:17:40 +0100

    Add files via upload
    optimized get_labels

--- prop2vec.py
+++ prop2vec.py
@@ -1,7 +1,9 @@
-import re
 import bz2
 import pandas as pd
 from gensim.models import KeyedVectors
+from itertools import islice
+import re
+import random.sample
 
 INFOBOXPROPS = 'infobox_properties_en.ttl.bz2'
 LABELS = 'labels_en.ttl.bz2'
@@ -32,27 +34,29 @@ def format_results_towords(ranked_tuples):
     return [format_Freebase_toTitle(t[0]) for t in ranked_tuples]
 
 
-def get_labels(p, n_basis=1000):
+def get_labels(p, max_labels=1000, formatted=True):
     ending = '/' + p + '>'
-    matching = pd.Series([ending in row for row in infoboxprops['property']])
-    objs = infoboxprops[matching]['subject'].drop_duplicates()
+    objs = (row[0] for row in zip(infoboxprops['subject'], infoboxprops['property']) if ending in row[1])
+    if max_labels is not 'all':
+        objs = list(islice(objs, max_labels))
     labels = labelled[labelled['subject'].isin(objs)].label
+    if formatted == True:
+        labels = labels.apply(format_DBP_toFreebase)
     return labels
 
 
-def get_w2v(word):
+def get_vector(word):
     try:
-        return model.wv[word]
+        return word_vectors[word]
     except:
         return None
 
 
-def similar_by_property(prop, n_results=10, n_basis=1000, justwords=False):
-    labels = get_labels(prop, n_basis=n_basis)
-    formatted_labels = labels.apply(format_DBP_toFreebase)
-    vectors = formatted_labels.apply(get_w2v).dropna()
-    pdcentroid = vectors.mean()
-    results = model.wv.similar_by_vector(pdcentroid, n_results)
+def similar_by_property(prop, n_results=10, max_labels=1000, justwords=False):
+    labels = get_labels(prop, max_labels=max_labels).tolist()
+    vectors = [get_vector(label) for label in labels if label in word_vectors]
+    centroid = pd.Series(vectors).mean()
+    results = word_vectors.similar_by_vector(centroid, n_results)
     if justwords == True:
         return format_results_towords(results)
     else:
@@ -65,17 +69,19 @@ if __name__ == "__main__":
     print('loading', LABELS)
     labelled = bz2_todf(LABELS, usecols=[0,2], colnames=['subject', 'label'])
 
-    property_list = infoboxprops.property.drop_duplicates().apply(lambda x: x.split('/')[-1][:-1]).tolist()
+    allproperties = set(infoboxprops.property.drop_duplicates().apply(lambda x: x.split('/')[-1][:-1]).tolist())
 
     print('building Word2vec model')
     model = KeyedVectors.load_word2vec_format(PRETRAINEDVECTORS, binary=True)
+    word_vectors = model.wv
+    del model
 
     while True:
         p = input('Enter a property: ')
-        if p in property_list:
+        if p in allproperties:
             results = similar_by_property(p)
             print('Results for '+p, *results, sep='\n')
         else:
-            sample_properties = random.sample(property_list, 3)
+            sample_properties = random.sample(allproperties, 3)
             print(p, 'is not a property.')
             print('You could try {1}, {2} or {3}.\n'.format(*sample_properties))
